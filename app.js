7c7d4680a31eeb414a9ef3ce66becaaaa54ef68d
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-05-06 17:30:14 +0100

    added Nav Bar functionality

--- app.js
+++ app.js
@@ -4,9 +4,16 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+document.getElementById('date').innerHTML = new Date().getFullYear();
+// date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
+navToggle.addEventListener('click', () => {
+  linksContainer.classList.toggle('show-links');
+});
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
