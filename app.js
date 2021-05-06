447038cab1da7907f1d0b5a6224c7ea9b3e03cbf
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-05-06 20:34:33 +0100

    added the scroll effects to the navBars, also added the floating home button, completed the calculation for the navBar scroll behavior's responsiveness

--- app.js
+++ app.js
@@ -12,9 +12,52 @@ const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', () => {
-  linksContainer.classList.toggle('show-links');
+  // linksContainer.classList.toggle('show-links');
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
 });
+const navBar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 // ********** fixed navbar ************
-
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navBar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add('fixed-nav');
+  } else {
+    navBar.classList.remove('fixed-nav');
+  }
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    // Prevent default link behavior
+    e.preventDefault();
+    // Navigate to specific spot
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    // Calculate the height
+    const navHeight = navBar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navBar.classList.contains('fixed-nav');
+    let position = document.getElementById(id).offsetTop - navHeight;
+    if (!fixedNav) position = position - navHeight;
+    if (navHeight > 82) position = position + containerHeight;
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
